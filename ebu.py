11cd5017dad7f06bd67ed0551adc59f9c7578ec0
Ankur Seth <iankurseth@gmail.com>
2025-11-23 23:17:49 +0530

    Now listing links from github blog

--- ebu.py
+++ ebu.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 import argparse
 
-from pyebu.utils import session
+from pyebu.utils import get_soup
 
 
 
 def get_args():
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("link", help="The main URL of the blog")
-	parser.add_argument("-l", "--listing", nargs="+", metavar="SELECTOR", help="CSS selectors for listing extraction")
-	parser.add_argument("-c", "--content", nargs="+", metavar="SELECTOR", help="CSS selectors for content extraction")
+	parser.add_argument("url", help="The main URL of the blog")
+	parser.add_argument("-c", "--container", nargs="+", metavar="SELECTOR", help="CSS selectors for listings container")
+	parser.add_argument("-i", "--items", nargs="+", metavar="SELECTOR", help="CSS selectors for listing items")
+	parser.add_argument("-l", "--link", nargs="+", metavar="SELECTOR", help="CSS selectors for listing item link")
+	parser.add_argument("-a", "--article", nargs="+", metavar="SELECTOR", help="CSS selectors for article content")
+
+	parser.add_argument("--page-start", type=int, default=1, help="First page number to process")
+	parser.add_argument("--page-end", type=int, default=1, help="Last page number to process")
 
 	parser.add_argument("-t", "--title", help="Optional title", default=None)
 	parser.add_argument("-d", "--description", help="Optional description", default=None)
@@ -21,6 +26,24 @@ def get_args():
 def main():
 	args = get_args()
 	print(args)
+	soup = get_soup(args.url)
+
+	container_selector = " ".join(args.container)
+	container = soup.select_one(container_selector)
+
+	items_selector = " ".join(args.items)
+	items = container.select(items_selector)
+
+	for i, item in enumerate(items, start=1):
+		if args.link:
+			selector = " ".join(args.link)
+			a = item.select_one(selector)
+			if a and a.name != "a":
+				a = a.find("a")
+		else:
+			a = item.find("a")
+		print(f"{i}. {a['href']}")
+		# break
 
 
 if __name__ == '__main__':
--- pyebu/utils.py
+++ pyebu/utils.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 session = requests.session()
+
+
+def get_page(url: str):
+	response = session.get(url)
+	return response
+
+def get_soup(url: str):
+	response = get_page(url)
+	soup = BeautifulSoup(response.text, "lxml")
+	return soup
 
